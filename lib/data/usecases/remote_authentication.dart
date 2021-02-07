ee15ccec0dcc15be519ec2960bf9bdf71a3ecc0d
Victor Vuelma <vuelma@farmarcas.com.br>
2021-02-07 20:03:49 -0300

    move api details from domain class to data class

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -14,10 +14,30 @@ class RemoteAuthentication {
   });
 
   Future<void> auth(AuthenticationParams params) async {
+    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
+
     await httpClient.request(
       url: url,
       method: 'post',
-      body: params.toJson(),
+      body: body,
     );
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    @required this.email,
+    @required this.password,
+  });
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams entity) =>
+      RemoteAuthenticationParams(
+        email: entity.email,
+        password: entity.secret,
+      );
+
+  Map toJson() => {'email': email, 'password': password};
 }
