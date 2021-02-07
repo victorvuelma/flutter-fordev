c7a88d32503b601594d3dec84c22524db2446a71
Victor Vuelma <vuelma@farmarcas.com.br>
2021-02-07 20:26:02 -0300

    ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -23,8 +23,10 @@ class RemoteAuthentication {
         method: 'post',
         body: body,
       );
-    } on HttpError {
-      throw DomainError.unexpected;
+    } on HttpError catch (error) {
+      throw error == HttpError.unauthorized
+          ? DomainError.invalidCredentials
+          : DomainError.unexpected;
     }
   }
 }
