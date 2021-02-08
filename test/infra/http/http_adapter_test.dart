de8b7c4f419b83ad1f9a80b45bbece51a0296974
Victor Vuelma <vuelma@farmarcas.com.br>
2021-02-07 22:09:52 -0300

    move SUT creation to the setup method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -26,12 +26,17 @@ class HttpAdapter {
 class ClientSpy extends Mock implements Client {}
 
 void main() {
+  HttpAdapter sut;
+  Client client;
+  String url;
+
+  setUp(() {
+    client = ClientSpy();
+    url = faker.internet.httpUrl();
+    sut = HttpAdapter(client);
+  });
   group('post', () {
     test('Should call post with correct values', () async {
-      final client = ClientSpy();
-      final url = faker.internet.httpUrl();
-      final sut = HttpAdapter(client);
-
       await sut.request(url: url, method: 'post');
 
       verify(client.post(
