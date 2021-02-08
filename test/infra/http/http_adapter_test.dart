cc61ac08be117232875801025677a93744296c38
Victor Vuelma <vuelma@farmarcas.com.br>
2021-02-07 22:34:25 -0300

    ensure HttpAdatapter returns null if post returns 204

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -100,5 +100,13 @@ void main() {
 
       expect(response, null);
     });
+
+    test('Should return null if post returns 204 with no data', () async {
+      mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
   });
 }
