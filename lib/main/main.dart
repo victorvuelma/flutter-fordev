4f7a335f64e9ecfe89e41ca6a9ac86a9bfa153d2
Victor Vuelma <vuelma@farmarcas.com.br>
2021-02-08 23:43:55 -0300

    add LoginPage

---
+++ lib/main/main.dart
@@ -0,0 +1,10 @@
+// Composition root
+// Main layer se acopla a todas, para elas não se acoplarem
+
+import 'package:flutter/material.dart';
+
+import '../ui/components/components.dart';
+
+void main() {
+  runApp(App());
+}
