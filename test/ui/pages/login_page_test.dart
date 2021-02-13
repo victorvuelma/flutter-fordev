04e181657119477cdfa20024881fdbad236c22d3
Victor Vuelma <vuelma@farmarcas.com.br>
2021-02-13 16:54:43 -0300

    ensure LoginPage hide loading

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -67,6 +67,8 @@ void main() {
 
     final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
     expect(button.onPressed, null);
+
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 
   testWidgets("Should call validate with correct values",
@@ -201,5 +203,16 @@ void main() {
     await tester.pump();
 
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets("Should hide loading", (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isLoadingController.add(true);
+    await tester.pump();
+    isLoadingController.add(false);
+    await tester.pump();
+
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 }
