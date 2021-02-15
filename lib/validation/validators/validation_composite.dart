189951d163581203388be5663336886abc25cac9
Victor Vuelma <vuelma@farmarcas.com.br>
2021-02-14 23:41:40 -0300

    move ValidationComposite to its own file

---
+++ lib/validation/validators/validation_composite.dart
@@ -0,0 +1,25 @@
+import 'package:meta/meta.dart';
+
+import '../../presentation/protocols/protocols.dart';
+import '../protocols/protocols.dart';
+
+class ValidationComposite implements Validation {
+  final List<FieldValidation> validations;
+
+  ValidationComposite(this.validations);
+
+  String validate({
+    @required String field,
+    @required String value,
+  }) {
+    for (final validation in validations.where((v) => v.field == field)) {
+      final error = validation.validate(value);
+
+      if (error?.isNotEmpty == true) {
+        return error;
+      }
+    }
+
+    return null;
+  }
+}
--- test/validaton/validators/validation_composite_test.dart
+++ test/validaton/validators/validation_composite_test.dart
@@ -1,30 +1,8 @@
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-import 'package:fordev/presentation/protocols/protocols.dart';
 import 'package:fordev/validation/protocols/protocols.dart';
-
-class ValidationComposite implements Validation {
-  final List<FieldValidation> validations;
-
-  ValidationComposite(this.validations);
-
-  String validate({
-    @required String field,
-    @required String value,
-  }) {
-    for (final validation in validations.where((v) => v.field == field)) {
-      final error = validation.validate(value);
-
-      if (error?.isNotEmpty == true) {
-        return error;
-      }
-    }
-
-    return null;
-  }
-}
+import 'package:fordev/validation/validators/validators.dart';
 
 class FieldValidationSpy extends Mock implements FieldValidation {}
 
