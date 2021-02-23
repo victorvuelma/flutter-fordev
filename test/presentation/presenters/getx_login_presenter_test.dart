2b7b56f0074ec42ea2f444bcaf08add10775c8ae
Victor Vuelma <vuelma@farmarcas.com.br>
2021-02-22 22:49:35 -0300

    ensure GetxLoginPresenter emits error if SaveCurrentAccount fails

--- test/presentation/presenters/getx_login_presenter_test.dart
+++ test/presentation/presenters/getx_login_presenter_test.dart
@@ -42,6 +42,13 @@ void main() {
     mockAutheticationCall().thenThrow(error);
   }
 
+  PostExpectation mockSaveCurrentAccountCall() =>
+      when(saveCurrentAccount.save(any));
+
+  void mockSaveCurrentAccountError() {
+    mockSaveCurrentAccountCall().thenThrow(DomainError.unexpected);
+  }
+
   setUp(() {
     validation = ValidationSpy();
     authentication = AuthenticationSpy();
@@ -157,6 +164,19 @@ void main() {
     await sut.auth();
 
     verify(saveCurrentAccount.save(AccountEntity(token))).called(1);
+  });
+
+  test('Should emit UnexpectedError if SaveCurrentAccount fails', () async {
+    mockSaveCurrentAccountError();
+
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+    sut.mainErrorStream.listen(expectAsync1((error) =>
+        expect(error, 'Algo errado aconteceu. Tente novamente em breve.')));
+
+    await sut.auth();
   });
 
   test('Should emit correct events on Authentication success', () async {
