ce96a1a2ddf01bd6b7f17d3d509627f2c37c1078
Victor Vuelma <vuelma@farmarcas.com.br>
2021-07-25 13:00:54 -0300

    ensure SignUpPage presents password error

--- test/ui/pages/signup_page_test.dart
+++ test/ui/pages/signup_page_test.dart
@@ -172,4 +172,25 @@ void main() {
       findsOneWidget,
     );
   });
+
+  testWidgets('Should present password error', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add(UiError.invalidField);
+    await tester.pump();
+    expect(find.text('Campo inválido'), findsOneWidget);
+
+    passwordErrorController.add(UiError.requiredField);
+    await tester.pump();
+    expect(find.text('Campo obrigatório'), findsOneWidget);
+
+    passwordErrorController.add(null);
+    await tester.pump();
+
+    expect(
+      find.descendant(
+          of: find.bySemanticsLabel('Senha'), matching: find.byType(Text)),
+      findsOneWidget,
+    );
+  });
 }
